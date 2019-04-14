4fdfcd4bf9f3b9dfe3ee3f6dabba6094909e9639
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-14 17:45:59 +0200

    Add JWT check

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const neo4j = require('neo4j-driver').v1
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 // const config = require('./config')
 
@@ -13,6 +15,19 @@ const router = express.Router()
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({extended: false})
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${process.env.login || config.auth0.domain}.well-known/jwks.json`,
+  }),
+
+  audience: process.env.client_id || config.auth0.client_id,
+  issuer: process.env.domain || config.auth0.domain,
+  algorithms: ['RS256'],
+})
+
 app.use(jsonParser)
 app.use(urlencodedParser)
 app.use(cors())
@@ -23,9 +38,9 @@ router.route('/users/:userid')
   .get(moviesController.getUserMovies)
 
 router.route('/movies/:imdbID')
-  .get(moviesController.getMovie)
-  .put(moviesController.putMovie)
-  .delete(moviesController.deleteMovie)
+  .get(checkJwt, moviesController.getMovie)
+  .put(checkJwt, moviesController.putMovie)
+  .delete(checkJwt, moviesController.deleteMovie)
 
 app.listen(process.env.PORT || 3001)
 
